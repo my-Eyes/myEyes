a4e520a65c861b4ac307dc6959f7f4d67d8797bb
Dani Rigsby <drigsby@hotmail.com>
2015-06-30 20:16:34 -0700

    profile page is almost complete as far as css is concerned, still need logic for the tour photos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @tours = Tour.all
+    @saved_tours = @user.saved_tours
+    @user_tours = @user.tours
   end
 end
